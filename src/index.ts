5de36ced8271225534d816f9d182efbd5cac5c58
Thomas Queiroz <thomasqueirozb@gmail.com>
2020-04-25 17:23:46 -0300

    Fixed bug: uninitialized route.controller in index.ts. Also changed ternary operator for a more convenient ||

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,9 @@ createConnection().then(async connection => {
     Routes.forEach(route => {
         app[route.method](
             route.route,                                          // Path: something like /users/:id
-            (route.handlers === undefined) ? [] : route.handlers, // Handlers: gets called before the function | normally auth
+            route.handlers || [],                                 // Handlers: gets called before the function | normally auth
             (req: Request, res: Response, next: Function) => {    // The actual funcion that handles the request
-                const result = route.controller[route.action](req, res, next);
+                const result = (new route.controller)[route.action](req, res, next);
                 if (result instanceof Promise) {
                     result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
                 } else if (result !== null && result !== undefined) {
